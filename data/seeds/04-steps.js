4648a2fab245289ef9c76aad31edcead7c317eb8
bryanbilek <you@example.com>
2020-04-30 15:02:24 -0500

    fixed a step update

--- data/seeds/04-steps.js
+++ data/seeds/04-steps.js
@@ -3,6 +3,6 @@ exports.seed = function (knex) {
     { step_number: 1, instructions: 'Preheat oven to 350 degrees', recipe_id: 1 },
     { step_number: 2, instructions: 'Mix flour and eggs', recipe_id: 1 },
     { step_number: 3, instructions: 'Put in oven', recipe_id: 1 },
-    { step_number: 1, instructions: 'Start grill', recipe_id: 1 }
+    { step_number: 1, instructions: 'Start grill', recipe_id: 2 }
   ]);
 };
