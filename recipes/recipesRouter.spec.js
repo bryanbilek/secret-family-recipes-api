32765e87d541f82a8975003c8e59bb7939564b78
bryanbilek <you@example.com>
2020-04-30 19:18:40 -0500

    testing

--- recipes/recipesRouter.spec.js
+++ recipes/recipesRouter.spec.js
@@ -1,69 +1,64 @@
 const request = require('supertest');
 const db = require('../data/dbConfig');
 const server = require('../api/server');
+const jwt = require('jsonwebtoken');
+jest.mock('../middleware/restricted', () => {
+    return (req, res, next) => {
+        next();
+    }
+})
 
-describe('server', function () {
-    test('should run the test', function () {
+describe('server/recipesRouter', function() {
+    test('should run the test', function() {
         expect(true).toBe(true);
     });
-});
 
-describe('CRUD reqs for /api/recipes', function () {
-    let token;
-    beforeEach(async () => {
-        const response = await test(server)
+    //login before running test on restricted router
+    beforeAll((done) => {
+        request(server)
             .post('/api/auth/login')
-            .send({ username: 'michael', password: 'jordan' })
-        token = response.body.token;
-        test(server)
-        .post('/api/auth/login')
-        .send({ username: 'michael', password: 'jordan' })
+            .send({ username: 'mj23', password: 'airjordan' })
+            .end((err, res) => {
+                token = res.body.token;
+                done();
+        });
     });
 
-    it('should return 200 ok', () => {
-        return request(server)
+    describe('GET /recipes', () => {
+        test('should return 200 status', async () => {
+            const res = await request(server)
             .get('/api/recipes')
-            .set({ Authorization: token })
-            .then(res => {
-                expect(res.status).toBe(200);
-            });
-    });
-});
+            .set('Authorization', token);
+            expect(res.status).toBe(200);
+        })
+    })
 
-// describe('server', function () {
-//     describe('All CRUD for recipesRouter', function () {
-//         //login with token so tests will be able to run 
-//         beforeAll((done) => {
-//             request(server)
-//                 .post('/api/auth/login')
-//                 .send({ username: 'michael', password: 'jordan' })
-//                 done();
-//         });
+    // describe('GET /recipes/:id', () => {
+    //     test('should return 200 status', async () => {
+    //         const res = await request(server)
+    //         .get('/api/recipes/1')
+    //         .set('Authorization', token)
+    //         expect(res.status).toBe(200);
+    //     })
+    // })
 
-//         beforeEach(async () => {
-//             // this function executes and clears out the table before each test
-//             await db('users').truncate();
-//         });
+    // describe('POST /recipes', () => {
+    //     test('should return 201 status', async () => {
+    //         const res = await request(server)
+    //         .post('/api/recipes')
+    //         .send({
+    //             id: 1,
+    //             user_id: 1,
+    //             recipe_name: "bake cookies",
+    //             description: "learn how to make great cookies",
+    //             prep_time: "7 minutes",
+    //             cook_time: "8 minutes",
+    //             serving_size: "4 people",
+    //             image_url: null
+    //         })
+    //         .set('Authorization', token)
+    //         expect(res.status).toBe(201);
+    //     })
+    // })
 
-
-//         // it('should find recipes 200 ok', async () => {
-//         //     let recipe = await Recipes.find();
-//         //     expect(res.status)toEqual(200);
-//         // });
-
-//         test('should return 200 ok with correct credentials', async () => {
-//             const res = await request(server)
-//             .get('/api/recipes')
-//             .set('Authorization', token)
-//             .then(res => {
-//                 expect(res.status).toBe(200);
-//             })
-//         });
-
-//         // it('should find recipes with success message', async () => {
-//         //     let recipe = await Recipes.find();
-//         //     expect('Success');
-//         // });
-
-//     });
-// });
+})
